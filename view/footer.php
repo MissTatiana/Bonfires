dcb5ed7746eabdb3576862081f3cdb2ebc4a4c21
Tatianae <Miss_Tati_K@yahoo.com>
2013-10-29 22:00:06 -0400

    footer finially finished

--- view/footer.php
+++ view/footer.php
@@ -8,15 +8,15 @@
 			<li><a href="?action=contact">Contact</a></li>
 		</ul><!-- footerNav -->
 	</div><!-- foot -->
+	<br />
 	
-	<div id="addressFooter">
-		<h3>Bonfires in Oviedo</h3>
-		<p>
-			1340 Alafaya Trail<br />
-			Ovideo Fl, 32765<br />
-			(407) 366 - 4073<br />
-		</p>
-	</div><!-- addressFooter -->
+	<div id="location">
+		<h4>Bonfires in Ovideo</h4>
+		
+		<p>1340 Alafaya Trail</p>
+		<p>Ovideo FL, 32765</p>
+		<p>(407) 366-4073</p>
+	</div><!-- location -->
 	
 	<div id="hours">
 		<p>Monday-Thursday - 11am-1am</p>
@@ -28,13 +28,13 @@
 	<div id="social">
 		
 		<!-- facebook -->
-		<a href="https://www.facebook.com/bonfiresoviedo"><img src="images/social/facebook.png" alt="Bonfires facebook link"/></a>
+		<a href="https://www.facebook.com/bonfiresoviedo"><img src="images/social/facebook.png" alt="Bonfires facebook link" width="50" height="50"/></a>
 		
 		<!-- twitter -->
-		<a href="https://twitter.com/Bonfiresoviedo"><img src="images/social/twitter.png" alt="Bonfires twitter link" /></a>
+		<a href="https://twitter.com/Bonfiresoviedo"><img src="images/social/twitter.png" alt="Bonfires twitter link" width="50" height="50" /></a>
 		
 		<!-- youtube -->
-		<a href="http://www.youtube.com/bonfiresbarandgrill"><img src="images/social/youtube.png" alt="Bonfires youtube link" /></a>
+		<a href="http://www.youtube.com/bonfiresbarandgrill"><img src="images/social/youtube.png" alt="Bonfires youtube link" width="50" height="50"/></a>
 		
 	</div>
 	
