a7cad76ac2b2aecb7973836987bb86c6f7d9c7bf
Tatianae <Miss_Tati_K@yahoo.com>
2013-11-03 14:01:58 -0500

    feedback done needs to float

--- view/contactContent.php
+++ view/contactContent.php
@@ -62,46 +62,70 @@
 		<form action="feedback">
 			
 			<label for="feedEmail">*Email Address:</label><br />
-			<input type="email" name="feedEmail" id="feedEmail" class="input" required="required" />
+			<input type="email" name="feedEmail" id="feedEmail" class="input" placeholder="example@email.com" required="required" /><br />
 			
 			<label for="food">Food:</label>
-			<div id="food">
-				<input type="radio" name="food" value="1"><p>1</p><br />
-				<input type="radio" name="food" value="2"><p>2</p><br />
-				<input type="radio" name="food" value="3"><p>3</p><br />
-				<input type="radio" name="food" value="4"><p>4</p><br />
-				<input type="radio" name="food" value="5"><p>5</p><br />
+			<div id="food" class="radioDiv">
+				<input type="radio" id="food1" class="radio" name="food" value="1">
+				<input type="radio" id="food2" class="radio" name="food" value="2">
+				<input type="radio" id="food3" class="radio" name="food" value="3">
+				<input type="radio" id="food4" class="radio" name="food" value="4">
+				<input type="radio" id="food5" class="radio" name="food" value="5">
+				<br />
+				<label for="food1">1</label>
+				<label for="food2">2</label>
+				<label for="food3">3</label>
+				<label for="food4">4</label>
+				<label for="food5">5</label>			
 			</div><!-- food -->
 			
 			<label for="service">Service:</label>
-			<div id="service">
-				<input type="radio" name="service" value="1"><p>1</p><br />
-				<input type="radio" name="service" value="2"><p>2</p><br />
-				<input type="radio" name="service" value="3"><p>3</p><br />
-				<input type="radio" name="service" value="4"><p>4</p><br />
-				<input type="radio" name="service" value="5"><p>5</p><br />
+			<div id="service" class="radioDiv">
+				<input type="radio" id="service1" class="radio" name="service" value="1">
+				<input type="radio" id="service2" class="radio" name="service" value="2">
+				<input type="radio" id="service3" class="radio" name="service" value="3">
+				<input type="radio" id="service4" class="radio" name="service" value="4">
+				<input type="radio" id="service5" class="radio" name="service" value="5">
+				<br />				
+				<label for="service1">1</label>
+				<label for="service2">2</label>
+				<label for="service3">3</label>
+				<label for="service4">4</label>
+				<label for="service5">5</label>
 			</div><!-- service -->
 			
 			<label for="cleaniness">Cleaniness:</label>
-			<div id="cleaniness">
-				<input type="radio" name="cleaniness" value="1"><p>1</p><br />
-				<input type="radio" name="cleaniness" value="2"><p>2</p><br />
-				<input type="radio" name="cleaniness" value="3"><p>3</p><br />
-				<input type="radio" name="cleaniness" value="4"><p>4</p><br />
-				<input type="radio" name="cleaniness" value="5"><p>5</p><br />
+			<div id="cleaniness" class="radioDiv">
+				<input type="radio" id="clean1" class="radio" name="cleaniness" value="1">
+				<input type="radio" id="clean2" class="radio" name="cleaniness" value="2">
+				<input type="radio" id="clean3" class="radio" name="cleaniness" value="3">
+				<input type="radio" id="clean4" class="radio" name="cleaniness" value="4">
+				<input type="radio" id="clean5" class="radio" name="cleaniness" value="5">
+				<br />
+				<label for="clean1">1</label>
+				<label for="clean2">2</label>
+				<label for="clean3">3</label>
+				<label for="clean4">4</label>
+				<label for="clean5">5</label>
 			</div><!-- cleaniness -->
 			
 			<label for="spev">Specials and Events:</label>
-			<div id="spev">
-				<input type="radio" name="spev" value="1"><p>1</p><br />
-				<input type="radio" name="spev" value="2"><p>2</p><br />
-				<input type="radio" name="spev" value="3"><p>3</p><br />
-				<input type="radio" name="spev" value="4"><p>4</p><br />
-				<input type="radio" name="spev" value="5"><p>5</p><br />
+			<div id="spev" class="radioDiv">
+				<input type="radio" id="spev1" class="radio" name="spev" value="1">
+				<input type="radio" id="spev2" class="radio" name="spev" value="2">
+				<input type="radio" id="spev3" class="radio" name="spev" value="3">
+				<input type="radio" id="spev4" class="radio" name="spev" value="4">
+				<input type="radio" id="spev5" class="radio" name="spev" value="5">
+				<br />				
+				<label for="spev1">1</label>
+				<label for="spev2">2</label>
+				<label for="spev3">3</label>
+				<label for="spev4">4</label>
+				<label for="spev5">5</label>
 			</div><!-- spev -->
 			
 			<label for="comments">Additional Comments:</label>
-			<textarea id="comments" name="comments"></textarea>
+			<textarea id="comments" name="comments"></textarea><br />
 			
 			<input type="submit" id="feedbackBtn" class="btn" value="Submit" />
 		
